3c1b1893c1a0999e550e0c7d19c9201e7ceb13ff
goueguy <jlagoueguy@gmail.com>
2021-06-01 19:52:10 +0000

    create offres pages

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,8 +2,31 @@
 @section('title',"ACCUEIL")
 @section('content')
 
-<p>HOME PAGE</p>
-
+<h1 class="py-10 text-4xl font-bold text-center text-green-700 ">NOS OFFRES</h1>
+<div class="flex flex-row items-center pl-14 pr-14">
+    <div class="flex flex-col flex-grow">
+        <div class="flex flex-row items-center w-3/4 p-3 bg-white shadow-lg">
+            <div class="w-10/12">
+                <h2 class="text-gray-400 uppercase">Formateur Sénior billingue</h2>
+                <p class="font-light">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa commodi illo nam in itaque corrupti aliquam accusamus, saepe sint libero accusantium veniam voluptatibus delectus consectetur illum impedit assumenda recusandae maiores!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa commodi illo nam in itaque corrupti aliquam accusamus, saepe sint libero accusantium veniam voluptatibus delectus consectetur illum impedit assumenda recusandae maiores!
+                </p>
+            </div>
+            <div class="">
+                <span class="p-3 text-white bg-green-800">STAGE</span>
+            </div>
+        </div>
+        <div>A</div>
+    </div>
+    <div>B</div>
+</div>
 @endsection
-
+@push('metier')
+<style>
+    body{
+        background-color: #f3f4f6;
+    }
+</style>
+@endpush
 
