4d181f6b7883247d5e20a778011ab9096374b7d6
goueguy <jlagoueguy@gmail.com>
2021-06-02 03:39:14 +0000

    make responsive connexion page

--- resources/views/frontend/connexion.blade.php
+++ resources/views/frontend/connexion.blade.php
@@ -2,10 +2,10 @@
 @section('title',"CONNEXION")
 @section('content')
 
-<div class="flex flex-row justify-between w-full">
-    <div class="relative">
+<div class="flex flex-row justify-between">
+    <div class="relative w-screen">
         <img src="{{asset('assets/images/home-photo.png')}}" class="bg-gray-500 bg-no-repeat" alt="">
-        <div class="absolute top-0 right-0 flex flex-col mt-24 mr-12">
+        <div class="absolute top-0 right-0 flex flex-col mt-20 mr-24">
             <div class="mx-auto text-center pt-36">
                 <div class="flex flex-row items-center p-4 bg-white border border-transparent rounded-lg shadow cursor-pointer w-90">
                     <img src="{{asset('/assets/images/google.svg')}}" class="w-6">
