9040746a3dcec573f3e5e9c1a7705797ac3d0000
goueguy <jlagoueguy@gmail.com>
2021-06-15 10:35:44 +0000

    apache authorized requests

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,7 +12,9 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array
      */
-    protected $policies = ['*'];
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
 
     /**
      * Register any authentication / authorization services.
