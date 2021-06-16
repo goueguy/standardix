6ab728f6adf60bb0883f60a23b12b3ef4cbb0400
goueguy <jlagoueguy@gmail.com>
2021-06-16 16:51:16 +0000

    remove focus on use dropdown

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -16,7 +16,7 @@
                 <a href="#" class="pr-8 font-bold uppercase"><span>Nos métiers</span></a>
                 <a href="#" class="pr-8 font-bold uppercase"><span>Contact</span></a>
                 @auth
-                    <button type="button" id="dropdown" href="{{(Auth::user()->niveau_acces==1 || Auth::user()->niveau_acces==2) ? route('admin.dashboard') : route('candidats.dashboard')}}" class="flex justify-center font-bold"><svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 dropdown dropdown:hover dropdown-menu:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <button type="button" id="dropdown" href="{{(Auth::user()->niveau_acces==1 || Auth::user()->niveau_acces==2) ? route('admin.dashboard') : route('candidats.dashboard')}}" class="focus:outline-none flex justify-center font-bold"><svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 dropdown dropdown:hover dropdown-menu:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg><span>{{Str::substr(Auth::user()->nom, 0, 1)}}{{Str::substr(Auth::user()->prenoms, 0, 1)}}.</span></button>
                 @else
