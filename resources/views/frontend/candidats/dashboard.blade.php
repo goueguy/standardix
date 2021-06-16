252228d91bf47d443dbc536d555601e4c91c1bca
goueguy <jlagoueguy@gmail.com>
2021-06-16 13:41:46 +0000

    update detail error with id

--- resources/views/frontend/candidats/dashboard.blade.php
+++ resources/views/frontend/candidats/dashboard.blade.php
@@ -39,7 +39,7 @@
                     <td class="p-2 text-sm border border-gray-400">{{date("d-m-Y",strtotime($offre->date_edition))}}</td>
                     <td class="p-2 text-sm border border-gray-400">{{date("d-m-Y",strtotime($offre->date_limite))}}</td>
                     <td class="p-2 border border-gray-400">
-                        <a href="{{route('candidatures.detail.offre',encrypt($offre->id))}}" class="flex text-red-500">
+                        <a href="{{route('candidatures.detail.offre',$offre->id)}}" class="flex text-red-500">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
